08e398efa6abb34ab74d111b5d2786ab13670e00
Richard Bakos <resonance.designs.com@gmail.com>
2025-08-08 08:50:09 -0600

    Added support for Atlas
    Added support for using the MongoDB Atlas service.

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,11 +2,20 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect(`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}?authSource=${process.env.MONGO_AUTH_SOURCE}`);
-        console.log('MongoDB established connection with host:', process.env.MONGO_HOST);
-        console.log('MongoDB is running on port:', process.env.MONGO_PORT);
-        console.log('MongoDB connected successfully to database:', process.env.MONGO_DB);
-        console.log("Server is ready to accept requests.");
+        if (process.env.ATLAS === 'no') {
+            // Connect to MongoDB using the provided credentials and host
+            await mongoose.connect(`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}?authSource=${process.env.MONGO_AUTH_SOURCE}`);
+            console.log('MongoDB established connection with host:', process.env.MONGO_HOST);
+            console.log('MongoDB is running on port:', process.env.MONGO_PORT);
+            console.log('MongoDB connected successfully to database:', process.env.MONGO_DB);
+            console.log("Server is ready to accept requests.");
+        } else if (process.env.ATLAS === 'yes') {
+            // Connect to MongoDB Atlas using the connection string
+            await mongoose.connect(process.env.MONGO_ATLAS_URI);
+            console.log('MongoDB is connected via atlas server');
+        } else {
+            throw new Error('Invalid ATLAS environment variable. Set it to "yes" or "no".');
+        }
     } catch (error) {
         console.error('MongoDB connection failed:', error);
         process.exit(1); // Exit the process with failure
